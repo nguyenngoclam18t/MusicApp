edf80a8ffb008a0def5c0b83d1fc70aa45d640a4
Nâmmm <95410079+nguyenngoclam18t@users.noreply.github.com>
2024-05-08 13:17:45 +0700

    update navbar

---
+++ app/src/main/java/com/example/musicapp/HomePage.java
@@ -0,0 +1,23 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+    void loadFragment(Fragment fragment){
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        ft.replace(R.id.FrameHomePage,fragment);
+        ft.commit();
+    }
+}
