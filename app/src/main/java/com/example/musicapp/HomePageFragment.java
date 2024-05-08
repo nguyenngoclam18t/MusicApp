ed6a69eb42f1cb52a0387670fbb0413b3d769c85
Nâmmm <95410079+nguyenngoclam18t@users.noreply.github.com>
2024-05-08 14:42:41 +0700

    update navbar add icon and fragment

--- app/src/main/java/com/example/musicapp/HomePageFragment.java
+++ app/src/main/java/com/example/musicapp/HomePageFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomePage#newInstance} factory method to
+ * Use the {@link HomePageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomePage extends Fragment {
+public class HomePageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class HomePage extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public HomePage() {
+    public HomePageFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class HomePage extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomePage.
+     * @return A new instance of fragment HomePageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomePage newInstance(String param1, String param2) {
-        HomePage fragment = new HomePage();
+    public static HomePageFragment newInstance(String param1, String param2) {
+        HomePageFragment fragment = new HomePageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
--- app/src/main/java/com/example/musicapp/Nav_Bar_Menu.java
+++ app/src/main/java/com/example/musicapp/Nav_Bar_Menu.java
@@ -1,23 +1,68 @@
 package com.example.musicapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Nav_Bar_Menu extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    FrameLayout framefragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_bar_menu);
+        loadFragment(new HomePageFragment());
+        Get();
+
     }
-    void loadFragment(Fragment fragment){
-        FragmentManager fm=getSupportFragmentManager();
-        FragmentTransaction ft=fm.beginTransaction();
-        ft.replace(R.id.FrameHomePage,fragment);
+
+    void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.FrameHomePage, fragment);
         ft.commit();
+    }
+
+    void Get() {
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navBottomBarMenu);
+        framefragment = (FrameLayout) findViewById(R.id.FrameHomePage);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.navigation_homepage) {
+                    loadFragment(new HomePageFragment());
+                    // Load Shop fragment or perform other actions
+                    return true;
+                } else if (id == R.id.navigation_top) {
+                    loadFragment(new HomePageFragment());
+
+                    // Load Top fragment or perform other actions
+                    return true;
+                } else if (id == R.id.navigation_genres) {
+                    loadFragment(new HomePageFragment());
+
+                    // Load Cart fragment or perform other actions
+                    return true;
+                } else if (id == R.id.navigation_Profile) {
+                    loadFragment(new HomePageFragment());
+
+                    // Load Profile fragment or perform other actions
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
