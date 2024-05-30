aa6f32adc3233a4e313a0806273ae22ae288e6ed
Nâmmm <95410079+nguyenngoclam18t@users.noreply.github.com>
2024-05-30 23:43:41 +0700

    update pull data from firebase store

--- app/src/main/java/com/example/musicapp/View/LoadingScreen.java
+++ app/src/main/java/com/example/musicapp/View/LoadingScreen.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.musicapp.Model.AlbumModel;
+import com.example.musicapp.Model.ArtistsModel;
 import com.example.musicapp.Model.FireStoreDB;
 import com.example.musicapp.Model.FirestoreCallback;
 import com.example.musicapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class LoadingScreen extends AppCompatActivity implements FirestoreCallback {
 
@@ -19,9 +24,20 @@ public class LoadingScreen extends AppCompatActivity implements FirestoreCallbac
     }
     @Override
     public void onCallback() {
-
+        preloadImages();
         Intent intent = new Intent(LoadingScreen.this, Nav_Bar_Menu.class);
         startActivity(intent);
         finish();
+    }
+    private  void preloadImages(){
+        for (AlbumModel model:FireStoreDB.arrAlbum ) {
+            loadImage(model.imgUrl);
+        }
+        for (ArtistsModel model:FireStoreDB.arrArtists ) {
+            loadImage(model.avatarUrl);
+        }
+    }
+    private void loadImage(String imageUrls) {
+        Picasso.get().load(imageUrls).fetch();
     }
 }
