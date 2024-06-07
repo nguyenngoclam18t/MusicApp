6b00056e00508c31bfab3e424d75c285793aa813
HoHuuTuong <163244900+HoHuuTuong@users.noreply.github.com>
2024-06-07 21:23:18 +0700

    Update Register.java

--- app/src/main/java/com/example/musicapp/View/Register.java
+++ app/src/main/java/com/example/musicapp/View/Register.java
@@ -2,103 +2,106 @@ package com.example.musicapp.View;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.musicapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
-    private boolean passwordShowing = false;
-    private boolean conPasswordShowing = false;
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonReg;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        final EditText email = findViewById(R.id.emailET);
-        final EditText mobile = findViewById(R.id.mobileET);
-
-        final EditText password = findViewById(R.id.passwordET);
-        final EditText conPassword = findViewById(R.id.conPasswordET);
-        final ImageView passwordIcon = findViewById(R.id.passwordIcon);
-        final ImageView conPasswordIcon = findViewById(R.id.conPasswordIcon);
-
-        final AppCompatButton signUpBtn = findViewById(R.id.signUpBtn);
-        final TextView signInBtn = findViewById(R.id.signInBtn);
-
-        passwordIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(passwordShowing){
-                    passwordShowing = false;
-
-                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    passwordIcon.setImageResource(R.drawable.password_show);
+            mAuth = FirebaseAuth.getInstance();
+            editTextEmail = findViewById(R.id.email);
+            editTextPassword = findViewById(R.id.password);
+            buttonReg = findViewById(R.id.btn_register);
+            progressBar = findViewById(R.id.progressBar);
+            textView = findViewById(R.id.loginNow);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    startActivity(intent);
+                    finish();
                 }
-                else{
-                    passwordShowing = true;
+            });
 
-                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    passwordIcon.setImageResource(R.drawable.password_hide);
+            buttonReg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    String email, password;
+                    email = String.valueOf(editTextEmail.getText());
+                    password = String.valueOf(editTextPassword.getText());
+
+                    if (TextUtils.isEmpty(email)) {
+                        Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (TextUtils.isEmpty(password)) {
+                        Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressBar.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+
+                                        Toast.makeText(Register.this, "Account created.",
+                                                Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(Register.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
                 }
-
-                password.setSelection(password.length());
-            }
-        });
-
-        conPasswordIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(conPasswordShowing){
-                    conPasswordShowing = false;
-
-                    conPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    conPasswordIcon.setImageResource(R.drawable.password_show);
-                }
-                else{
-                    conPasswordShowing = true;
-
-                    conPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    conPasswordIcon.setImageResource(R.drawable.password_hide);
-                }
-
-                conPassword.setSelection(conPassword.length());
-            }
-        });
-
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String getMobileTxt = mobile.getText().toString();
-                final String getEmailTxt = email.getText().toString();
-                Intent intent = new Intent(Register.this, OTPVerification.class);
-                intent.putExtra("mobile", getMobileTxt);
-                intent.putExtra("email", getEmailTxt);
-                startActivity(intent);
-            }
-        });
-
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+            });
+        };
     }
-}
