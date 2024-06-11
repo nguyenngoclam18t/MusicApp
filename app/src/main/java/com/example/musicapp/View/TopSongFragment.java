80bd4721ce61c925e1e6a1e7a0b07ff43c0ecf3f
Nâmmm <95410079+nguyenngoclam18t@users.noreply.github.com>
2024-06-11 18:15:04 +0700

    update

--- app/src/main/java/com/example/musicapp/View/TopSongFragment.java
+++ app/src/main/java/com/example/musicapp/View/TopSongFragment.java
@@ -18,7 +18,6 @@ import com.example.musicapp.Controller.TopMusicHomePageAdapter;
 import com.example.musicapp.Model.ApiCollectionHomePage;
 import com.example.musicapp.Model.ArtistsModel;
 import com.example.musicapp.Model.OnSongClick;
-import com.example.musicapp.Model.PlaylistModel;
 import com.example.musicapp.Model.SongModel;
 import com.example.musicapp.Model.ZingMp3Api;
 import com.example.musicapp.R;
@@ -37,18 +36,29 @@ import java.util.Date;
  */
 public class TopSongFragment extends Fragment implements OnSongClick {
 
-    private RecyclerView recyclerViewTopSongFragment;
-    private TextView titlePlayList;
-    private ImageView img;
-    private ZingMp3Api zingMp3Api = new ZingMp3Api();
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    RecyclerView recyclerViewTopSongFragment;
+    String ablumid;
+    TextView titlePlayList;
+    ImageView img;
+    ZingMp3Api zingMp3Api=new ZingMp3Api();
     public TopSongFragment() {
         // Required empty public constructor
     }
 
+
     public static TopSongFragment newInstance(String param1, String param2) {
         TopSongFragment fragment = new TopSongFragment();
         Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,11 +67,12 @@ public class TopSongFragment extends Fragment implements OnSongClick {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
-    public void getTopToRecycleView() {
+    public void getTopToRecycleView(){
         Bundle bundle = getArguments();
         String albumId;
         if (bundle != null) {
@@ -71,27 +82,27 @@ public class TopSongFragment extends Fragment implements OnSongClick {
         }
         new Thread(() -> {
             try {
-                ArrayList<SongModel> arrTop = new ArrayList<>();
+                ArrayList<SongModel> arrTop=new ArrayList<>();
                 JsonObject songData = zingMp3Api.getDetailPlaylist(albumId);
                 JsonObject items = songData.getAsJsonObject("data");
 
-                String titlePlaylist = items.get("title").getAsString();
-                String thumbnailMPlayList = items.get("thumbnailM").getAsString();
+                String titlePlaylist=items.get("title").getAsString();
+                String thumbnailMPlayList=items.get("thumbnailM").getAsString();
                 JsonObject Song = items.getAsJsonObject("song");
                 JsonArray arrSong = Song.getAsJsonArray("items");
-                for (JsonElement element : arrSong) {
+                for (JsonElement element :arrSong){
                     JsonObject itemObj = element.getAsJsonObject();
 
-                    String encodeId = itemObj.get("encodeId").getAsString();
-                    String title = itemObj.get("title").getAsString();
-                    String thumbnailM = itemObj.get("thumbnailM").getAsString();
-                    String artistsNames = itemObj.get("artistsNames").getAsString();
+                    String encodeId= itemObj.get("encodeId").getAsString();
+                    String title= itemObj.get("title").getAsString();
+                    String thumbnailM= itemObj.get("thumbnailM").getAsString();
+                    String artistsNames= itemObj.get("artistsNames").getAsString();
                     long timestamp = itemObj.get("releaseDate").getAsLong();
                     Date date = new Date(timestamp * 1000);
                     SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-                    String dateString = sdf.format(date);
                     int duration = itemObj.get("duration").getAsInt();
-                    arrTop.add(new SongModel(encodeId, title, artistsNames,  thumbnailM,"", duration));
+                    String dateString = sdf.format(date);
+                    arrTop.add(new SongModel(encodeId,title,artistsNames,thumbnailM,"",duration));
                 }
                 if (getActivity() != null) {
                     getActivity().runOnUiThread(() -> {
@@ -101,7 +112,7 @@ public class TopSongFragment extends Fragment implements OnSongClick {
                                 .into(img);
                         titlePlayList.setText(titlePlaylist);
                         RecyclerView.Adapter adapterTop;
-                        adapterTop = new TopMusicHomePageAdapter(arrTop, this);
+                        adapterTop=new TopMusicHomePageAdapter(arrTop,this);
                         recyclerViewTopSongFragment.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
                         recyclerViewTopSongFragment.setAdapter(adapterTop);
                     });
@@ -110,30 +121,20 @@ public class TopSongFragment extends Fragment implements OnSongClick {
                 e.printStackTrace();
             }
         }).start();
-    }
 
+
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_top_song, container, false);
-        recyclerViewTopSongFragment = view.findViewById(R.id.RecyclerViewTopSongFragment);
-        img = view.findViewById(R.id.imageBackground);
-        titlePlayList = view.findViewById(R.id.TitlePlaylistTopSong);
+        View view=inflater.inflate(R.layout.fragment_top_song, container, false);
+        recyclerViewTopSongFragment = (RecyclerView) view.findViewById(R.id.RecyclerViewTopSongFragment);
+        img=(ImageView)view.findViewById(R.id.imageBackground);
+        titlePlayList=(TextView) view.findViewById(R.id.TitlePlaylistTopSong);
 
-        if (getArguments() != null) {
-            updateData(getArguments());
-        }
-
+        getTopToRecycleView();
         return view;
     }
-
-    public void updateData(Bundle bundle) {
-        if (bundle != null) {
-            String albumId = bundle.getString("albumId");
-            getTopToRecycleView();
-        }
-    }
-
     @Override
     public void onSongClick(SongModel song) {
         Intent intent = new Intent(getContext(), PlayerActivity.class);
